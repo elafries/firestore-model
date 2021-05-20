d1cdf3e18d3a98ea775337dd91a638c3755eaa2b
Balazs Bereczky <bereczkybalazs1@gmail.com>
2021-05-20 11:09:09 +0200

    add update by id

--- src/Queries/Update.php
+++ src/Queries/Update.php
@@ -2,7 +2,30 @@
 
 namespace Elafries\FirestoreModel\Queries;
 
+use function array_keys;
+
 trait Update
 {
+    public function updateById(string $id, array $updateValues): void
+    {
+        $document = $this->getCollection()->document($id);
+        $updatableItem = $document->snapshot()->data();
 
+        foreach ($updateValues as $key => $updateValue) {
+            $updatableItem[$key] = $updateValue;
+        }
+
+        $document->update($this->getUpdateItem($updatableItem));
+    }
+
+    protected function getUpdateItem(array $item): array
+    {
+        $secureItem = $this->getSecureWriteItem($item);
+
+        return array_map(function ($propertyKey) use ($secureItem) {
+            return [
+                'path' => $propertyKey, 'value' => $secureItem[$propertyKey]
+            ];
+        }, array_keys($secureItem));
+    }
 }
