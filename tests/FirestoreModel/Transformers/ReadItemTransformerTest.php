11dd3d6346d3f05f17f88d2310497209410b7e0a
Balázs Bereczky <bereczkybalazs1@gmail.com>
2021-06-15 16:50:23 +0200

    read item tests

--- tests/FirestoreModel/Transformers/ReadItemTransformerTest.php
+++ tests/FirestoreModel/Transformers/ReadItemTransformerTest.php
@@ -3,12 +3,13 @@
 namespace Tests\FirestoreModel\Transformers;
 
 use Elafries\FirestoreModel\Transformers\ReadItemTransformer;
-use Illuminate\Hashing\BcryptHasher;
-use Kreait\Firebase\Firestore;
 use Monolog\Test\TestCase;
 
 class Model {
     use ReadItemTransformer;
+
+    protected $fillable = ['name', 'email'];
+    protected $hidden = ['password'];
 }
 
 class ReadItemTransformerTest extends TestCase
@@ -20,9 +21,35 @@ class ReadItemTransformerTest extends TestCase
         $this->model = new Model();
     }
 
-    public function test_on_empty_item()
+    public function test_on_empty_items()
     {
         $response = $this->model->getSecureReadItem([]);
+        $this->assertEmpty($response);
+    }
+
+    public function test_on_fillable_item()
+    {
+        $response = $this->model->getSecureReadItem(['name' => 'asd']);
+
+        $this->assertCount(1, $response);
+    }
+
+    public function test_on_fillable_item_with_a_non_fillable_item()
+    {
+        $response = $this->model->getSecureReadItem(['name' => 'asd', 'jozsi' => '2a']);
+
+        $this->assertCount(1, $response);
+    }
+
+    public function test_on_id()
+    {
+        $response = $this->model->getSecureReadItem(['id' => '2a']);
+        $this->assertCount(1, $response);
+    }
+
+    public function test_on_hidden_item()
+    {
+        $response = $this->model->getSecureReadItem(['password' => 'asd']);
         $this->assertEmpty($response);
     }
 }
